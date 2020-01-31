66fc4074ec4077343108135e3455ffeae1544d03
Justin <justinrh97@gmail.com>
2020-01-31 13:29:16 -0500

    Creating basic site layout and navigation

---
+++ our-app/src/components/common/GlobalToolbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+
+function GlobalToolbar() {
+    return (
+        <Navbar bg="dark" variant="dark">
+            <Navbar.Brand href="/">
+                Block
+                <sub>Rx</sub>
+            </Navbar.Brand>
+            <Nav className="mr-auto">
+                <Nav.Link href="home">Home</Nav.Link>
+                <Nav.Link href="pricing">Pricing</Nav.Link>
+                <Nav.Link href="about">About Us</Nav.Link>
+            </Nav>
+            <Button variant="outline-info" href="login">Login</Button>
+        </Navbar>
+    );
+}
+
+export default GlobalToolbar;
