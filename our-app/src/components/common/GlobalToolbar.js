44db7e67662e579cb800620b71c193255a1bc4f1
Justin <justinrh97@gmail.com>
2020-02-03 09:52:42 -0500

    I like single quotes

--- our-app/src/components/common/GlobalToolbar.js
+++ our-app/src/components/common/GlobalToolbar.js
@@ -3,17 +3,17 @@ import { Navbar, Nav, Button } from 'react-bootstrap';
 
 function GlobalToolbar() {
     return (
-        <Navbar bg="dark" variant="dark">
-            <Navbar.Brand href="/">
+        <Navbar bg='dark' variant='dark'>
+            <Navbar.Brand href='/'>
                 Block
                 <sub>Rx</sub>
             </Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="home">Home</Nav.Link>
-                <Nav.Link href="pricing">Pricing</Nav.Link>
-                <Nav.Link href="about">About Us</Nav.Link>
+            <Nav className='mr-auto'>
+                <Nav.Link href='home'>Home</Nav.Link>
+                <Nav.Link href='pricing'>Pricing</Nav.Link>
+                <Nav.Link href='about'>About Us</Nav.Link>
             </Nav>
-            <Button variant="outline-info" href="login">Login</Button>
+            <Button variant='outline-info' href='login'>Login</Button>
         </Navbar>
     );
 }
--- our-app/src/components/common/LocalToolbar.js
+++ our-app/src/components/common/LocalToolbar.js
@@ -3,12 +3,12 @@ import { Navbar, Nav } from 'react-bootstrap';
 
 function LocalToolbar() {
     return (
-        <Navbar bg="info" variant="light" sticky="top">
-            <Navbar.Brand href="">Username</Navbar.Brand>
-            <Nav className="mr-auto">
-                <Nav.Link href="dashboard">Dashboard</Nav.Link>
-                <Nav.Link href="records">My Records</Nav.Link>
-                <Nav.Link href="profile">My Profile</Nav.Link>
+        <Navbar bg='info' variant='light' sticky='top'>
+            <Navbar.Brand>Username</Navbar.Brand>
+            <Nav className='mr-auto'>
+                <Nav.Link href='dashboard'>Dashboard</Nav.Link>
+                <Nav.Link href='records'>My Records</Nav.Link>
+                <Nav.Link href='profile'>My Profile</Nav.Link>
             </Nav>
         </Navbar>
     );
