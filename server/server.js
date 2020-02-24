e45626d6f6ebedd193e93953b668abd8e0fe1ca0
David Michael <davidjimichael@gmail.com>
2020-02-24 14:33:38 -0500

    added proxy and auth func

--- server/server.js
+++ server/server.js
@@ -2,11 +2,12 @@ var body_parser = require("body-parser");
 var express = require("express");
 var db = require("./db");
 var router = require("./routes/index");
+var auth = require("./middleware/auth").auth;
 
 var app = express();
 app.use(body_parser.json()); // for parsing application/json
 app.use(body_parser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
-
+app.use(auth(_ => true));
 // for testing only
 app.get("/", (req, res, next) => {
     res.sendFile(__dirname + "/index.html");
