cd4ff07c0587a691bd931c46e7153db0a46c35a7
David Michael <davidjimichael@gmail.com>
2020-02-24 10:32:13 -0500

    removed server attempt

--- server/middleware/logger.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (options) {
-    return function (req, res, next) {
-        if (options.debug) {
-            console.log(req.method, req.hostname, req.url);
-        }
-        next();
-    }
-}
