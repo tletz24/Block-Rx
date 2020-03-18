5a2aac77440ab4f6912d747d238a6675e2ffa44c
manish <manishshiwlani@gmail.com>
2020-03-17 22:33:37 -0400

    made changes to homepage. added some pictures.

--- our-app/src/components/unauthenticated/HomePage.js
+++ our-app/src/components/unauthenticated/HomePage.js
@@ -1,9 +1,82 @@
 import React from 'react';
+import { Carousel } from 'react-bootstrap';
 
-function HomePage() {
+export default function HomePage() {
+    const mystyle = {
+        color: "Black",
+        fontFamily: "Helvetica"
+    };
     return (
-        <h1>WELCOME</h1>
+        <div>
+            <h1 style={mystyle}><b>Welcome to Health Wallet</b></h1>
+            <Carousel>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization.jpg"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization1.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization2.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization3.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization4.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization5.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization6.png"
+                    />
+                </Carousel.Item>
+                <Carousel.Item>
+                    <img
+                        width={800}
+                        height={450}
+                        alt="Welcome to Health Wallet"
+                        src="/server/immunization7.png"
+                    />
+                </Carousel.Item>
+
+            </Carousel>
+        </div >
     );
 }
 
-export default HomePage;
