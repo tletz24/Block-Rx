eb6f2a803aacfee84e5cc64abda21155776c5338
Manish <manish@Manishs-MacBook-Pro.local>
2020-04-02 17:08:32 -0400

    Some final changes to align the pictures

--- our-app/src/components/unauthenticated/HomePage.js
+++ our-app/src/components/unauthenticated/HomePage.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import './HomePage.css';
-import { MDBMask, MDBView, MDBContainer, MDBRow, MDBCol } from "mdbreact";
 
 function HomePage() {
     return (
         <div>
             <header>
-                <section className="et-hero-tabs">
+                <div className="et-hero-tabs">
                     <h1>We are Health Wallet</h1>
                     <h3>Our Goal is to provide a simple yet effective platform to keep track of one's immunization records</h3>
                     <div className="et-hero-tabs-container">
@@ -16,74 +15,62 @@ function HomePage() {
                         <a className="et-hero-tab" href="#our-team">Our Team</a>
                         <span className="et-hero-tab-slider"></span>
                     </div>
-                </section>
+                </div>
             </header>
             <main className="et-main">
-                <React.Fragment>
-                    <section className="et-slide" id="about-us">
-                        <h1>About Us</h1>
-                        <h3>We are an Electrical Engineering and Computer Science Senior Design Group at the University of Toledo.
-                        Our goal as part of the college of engineering is to come up with & implement a senior design project that relates to our field of study.
+                <div className="et-slide" id="about-us">
+                    <h1>About Us</h1>
+                    <h3>We are an Electrical Engineering and Computer Science Senior Design Group at the University of Toledo.
+                    Our goal as part of the college of engineering is to come up with & implement a senior design project that relates to our field of study.
                     Our team is comprised of all Computer Science & Engineering (CSE) Majors; therefore our project deals exclusively with Computer Science & Software Engineering.</h3>
-                    </section>
-                </React.Fragment>
-                <React.Fragment>
-                    <section className="et-slide" id="testimonials">
-                        <h1>Our Mission</h1>
-                        <h3>Our mission is to create a better platform for the collection, storage and access of immunization records that will ultimately help everyone that takes part in the immunization process from doctors to patients. Our platform will be simple to use & available to all who participate.
+                </div>
+                <div className="et-slide" id="testimonials">
+                    <h1>Our Mission</h1>
+                    <h3>Our mission is to create a better platform for the collection, storage and access of immunization records that will ultimately help everyone that takes part in the immunization process from doctors to patients. Our platform will be simple to use & available to all who participate.
                         Finally, our platform will also provide some added benefits for the government agencies that have a had in the immunization process.</h3>
-                    </section>
-                </React.Fragment>
-                <React.Fragment>
-                    <section className="et-slide" id="technology">
-                        <h1>Technology</h1>
-                        <h3>The technology that we will be using to build this platfrom spans from cloud computing, the latest and greatest in front end development, and the new & emerging blockchain technology.
-                        With cloud computing, we can build our very resource-smart & scalable ecosystem for which to build our platform.
-                        Using the latest front-end technologies we can provide a very simple to understand & effective user experice.
+                </div>
+                <div className="et-slide" id="technology">
+                    <h1>Technology</h1>
+                    <h3>The technology that we will be using to build this platfrom spans from cloud computing, the latest and greatest in front end development, and the new & emerging blockchain technology.
+                    With cloud computing, we can build our very resource-smart & scalable ecosystem for which to build our platform.
+                    Using the latest front-end technologies we can provide a very simple to understand & effective user experice.
                     Finally, with the use of blockchain, we have an unparalleled level of security and tracability to the data which we consume.</h3>
-                    </section>
-                </React.Fragment>
-                <React.Fragment>
-                    <section className="et-slide" id="our-team">
-                        <h1>Our Team</h1>
-                        <MDBContainer className="mt-5">
-                            <MDBRow className="mt-4">
-                                <MDBCol md="4">
-                                    <img
-                                        src={process.env.PUBLIC_URL + '/images/manish.png'} className="img-fluid" alt="Manish"
-                                    />
-                                </MDBCol>
-                            </MDBRow>
-                            <MDBRow className="mb-4">
-                                <MDBCol md="4">
-                                    <img
-                                        src={process.env.PUBLIC_URL + '/images/justin.png'} alt="Justin"
-                                        className="img-fluid"
-                                    />
-                                    <p className="white-text">Justin</p>
-                                </MDBCol>
-                            </MDBRow>
-                            <MDBRow className="mb-4">
-                                <MDBCol md="4">
-                                    <img
-                                        src={process.env.PUBLIC_URL + '/images/trenton.png'} alt="Trenton"
-                                        className="img-fluid"
-                                    />
-                                    <p className="white-text">Justin</p>
-                                </MDBCol>
-                            </MDBRow>
-                            <MDBRow className="mb-4">
-                                <MDBCol md="4">
-                                    <img
-                                        src={process.env.PUBLIC_URL + '/images/david.png'} alt="David"
-                                        className="img-fluid"
-                                    />
-                                    <p className="white-text">Justin</p>
-                                </MDBCol>
-                            </MDBRow>
-                        </MDBContainer>
-                    </section>
-                </React.Fragment>
+                </div>
+
+
+                <div className="et-slide" id="our-team">
+                    <h1>Our Team</h1>
+                    <div className="flex">
+                        <div>
+                            <img
+                                src={process.env.PUBLIC_URL + '/images/manish.png'} alt="Manish"
+                            />
+                            <p>Manish Shiwlani</p>
+                        </div>
+                        <div>
+                            <img
+                                src={process.env.PUBLIC_URL + '/images/justin.png'} alt="Justin"
+
+                            />
+                            <p>Justin Hinkle</p>
+                        </div>
+                        <div>
+                            <img
+                                src={process.env.PUBLIC_URL + '/images/trenton.png'} alt="Trenton"
+
+                            />
+                            <p>Trenton Letz</p>
+                        </div>
+                        <div>
+
+                            <img
+                                src={process.env.PUBLIC_URL + '/images/david.png'} alt="David"
+
+                            />
+                            <p>David Michael</p>
+                        </div>
+                    </div>
+                </div>
             </main>
         </div >
     );
