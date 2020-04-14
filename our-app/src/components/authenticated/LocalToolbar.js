5cd30826eea8920c64d1350afc40d0465c3edbd5
Manish <manish@Manishs-MacBook-Pro.local>
2020-04-14 19:11:13 -0400

    added ImmunizationForm to navigation bar for now.

--- our-app/src/components/authenticated/LocalToolbar.js
+++ our-app/src/components/authenticated/LocalToolbar.js
@@ -9,6 +9,7 @@ function LocalToolbar(props) {
                 <Nav.Link onClick={() => props.history.push('dashboard')}>Dashboard</Nav.Link>
                 <Nav.Link onClick={() => props.history.push('records')}>My Records</Nav.Link>
                 <Nav.Link onClick={() => props.history.push('profile')}>My Profile</Nav.Link>
+                <Nav.Link onClick={() => props.history.push('immunization')}>Immunization Form</Nav.Link>
             </Nav>
         </Navbar>
     );
