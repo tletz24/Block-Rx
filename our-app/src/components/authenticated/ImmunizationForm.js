adef4f86787760751be3a4fd1b271c032ed5305d
manish <manishshiwlani123@gmail.com>
2020-04-16 17:21:21 -0400

    added rest of the fields needed for immunization form

--- our-app/src/components/authenticated/ImmunizationForm.js
+++ our-app/src/components/authenticated/ImmunizationForm.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { Form, Button, Card } from 'react-bootstrap';
 
 
-
 const ImmunizationForm = (props) => {
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -39,9 +38,7 @@ const ImmunizationForm = (props) => {
 
 
         }
-        props.signup(immunization, props.history);
     };
-
     return (
         <Card>
             <Card.Header as="h5">Health Wallet Sign-Up</Card.Header>
@@ -65,7 +62,6 @@ const ImmunizationForm = (props) => {
                         <Form.Label>Contraindications</Form.Label>
                         <Form.Control type='contraindications' placeholder='Password' />
                     </Form.Group>
-
                     <Form.Group controlId='eligibility'>
                         <Form.Label>Eligibility</Form.Label>
                         <Form.Control type='eligibility' placeholder='Eligibility' />
@@ -81,7 +77,101 @@ const ImmunizationForm = (props) => {
                         <Form.Control type='exceptionReason' as="Exception Reason">
                         </Form.Control>
                     </Form.Group>
-
+                    <Form.Group controlId='orgOrigination'>
+                        <Form.Label>Org Origination</Form.Label>
+                        <Form.Control type='orgOrigination' as="Org Origination">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='orgSubmitter'>
+                        <Form.Label>Org Submitter</Form.Label>
+                        <Form.Control type='orgSubmitter' as="Org Submitter">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='admitDate'>
+                        <Form.Label>Admit Date</Form.Label>
+                        <Form.Control type='admitDate' as="Admit Date">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='vaccinationIdSubmitting'>
+                        <Form.Label>Vaccination Id Submitting</Form.Label>
+                        <Form.Control type='vaccinationIdSubmitting' as="Vaccination Id Submitting">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='eventType'>
+                        <Form.Label>Event Type</Form.Label>
+                        <Form.Control type='eventType' as="Event Type">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='providerPersonName'>
+                        <Form.Label>Provider Person Name</Form.Label>
+                        <Form.Control type='providerPersonName' as="Provider Person Name">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='providerPersonSuffix'>
+                        <Form.Label>Provider Person Suffix</Form.Label>
+                        <Form.Control type='providerPersonSuffix' as="Provider Person Suffix">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='doseVolume'>
+                        <Form.Label>Dose Volume</Form.Label>
+                        <Form.Control type='doseVolume' as="Dose Volume">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='doseVolumeUnits'>
+                        <Form.Label>Dose Volume Units</Form.Label>
+                        <Form.Control type='doseVolumeUnits' as="Dose Volume Units">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='expirationDate'>
+                        <Form.Label>Expiration Date</Form.Label>
+                        <Form.Control type='expirationDate' as="Expiration Date">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='fundingSource'>
+                        <Form.Label>Funding Source</Form.Label>
+                        <Form.Control type='fundingSource' as="Funding Source">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='infoStatementPubDate'>
+                        <Form.Label>Info Statement PubDate</Form.Label>
+                        <Form.Control type='infoStatementPubDate' as="Info Statement PubDate">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='infoStatementDate'>
+                        <Form.Label>Info Statement Date</Form.Label>
+                        <Form.Control type='infoStatementDate' as="Info Statement Date">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='lotNum'>
+                        <Form.Label>Lot Num</Form.Label>
+                        <Form.Control type='lotNum' as="Lot Num">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='mfgName'>
+                        <Form.Label>Mfg Name</Form.Label>
+                        <Form.Control type='mfgName' as="Mfg Name">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='orderingProvider'>
+                        <Form.Label>Ordering Provider</Form.Label>
+                        <Form.Control type='orderingProvider' as="Ordering Provider">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='product'>
+                        <Form.Label>Product</Form.Label>
+                        <Form.Control type='product' as="Product">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='routeOfAdmin'>
+                        <Form.Label>Route Of Admin</Form.Label>
+                        <Form.Control type='routeOfAdmin' as="Route Of Admin">
+                        </Form.Control>
+                    </Form.Group>
+                    <Form.Group controlId='siteOfAdmin'>
+                        <Form.Label>Site Of Admin</Form.Label>
+                        <Form.Control type='siteOfAdmin' as="Site Of Admin">
+                        </Form.Control>
+                    </Form.Group>
                     <Button variant='primary' type='submit'>Submit</Button>
                 </Form>
             </Card.Body>
