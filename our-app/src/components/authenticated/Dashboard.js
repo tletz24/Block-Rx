cee83eb8e089cf41cec6c1eebd7711a43e66363d
Justin <justinrh97@gmail.com>
2020-04-28 21:26:25 -0400

    Add name to dashboard

--- our-app/src/components/authenticated/Dashboard.js
+++ our-app/src/components/authenticated/Dashboard.js
@@ -1,22 +1,23 @@
 import React from 'react';
-import { Card, ListGroup, Row, Col, Container} from 'react-bootstrap';
-import  ImmunizationList from '../common/ImmunizationList'
+import { connect } from 'react-redux';
+import { Card, ListGroup, Row, Col, Container } from 'react-bootstrap';
+import ImmunizationList from '../common/ImmunizationList'
 
-function Dashboard() {
+function Dashboard(props) {
     return (
-        <Container fluid="xl" style={{height: '80rem', width: '90rem'}}>
+        <Container fluid="xl" style={{ height: '80rem', width: '90rem' }}>
             <Row>
                 <Col>
                     <Card>
                         <Card.Header as="h1">Dashboard</Card.Header>
                         <ListGroup variant="flush">
                             <ListGroup.Item>
-                                <h2></h2>
+                                <h2>{props.name}</h2>
                             </ListGroup.Item>
                             <ListGroup.Item>
                                 <h2>Vaccines</h2>
                                 <ImmunizationList
-                                    // TODO Insert Vaccines Here
+                                // TODO Insert Vaccines Here
                                 />
                             </ListGroup.Item>
                         </ListGroup>
@@ -27,4 +28,11 @@ function Dashboard() {
     );
 }
 
-export default Dashboard;
+const mapStateToProps = (state) => {
+    const user = state.authentication.user;
+    return {
+        name: user.firstName + ' ' + user.lastName
+    }
+};
+
+export default connect(mapStateToProps)(Dashboard);
