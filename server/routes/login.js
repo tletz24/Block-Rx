775326f9371ed5d7a32f99367ee0700e3e10d49e
David Michael <davidjimichael@gmail.com>
2020-04-30 15:45:18 -0400

    Revert "Fixed Login"
    This reverts commit c02447ccf35255c908b8c8fee51940f4628e9b1f.

--- server/routes/login.js
+++ server/routes/login.js
@@ -9,13 +9,13 @@ router.post("/", async (req, res) => {
             if (err) res.status(500).json(err);
             if (!user) res.status(404).json({ message: "User Not Found" });
 
-            const ok = user.checkPassword(req.body.password);
+            // const ok = user.checkPassword(req.body.password);
 
-            // do not return password property to client
-            delete user.password;
+            // // do not return password property to client
+            // delete user.password;
 
-            if (ok) res.status(200).json(user);
-            else res.status(401).json({ message: "Incorrect Password" });
+            // if (ok) res.status(200).json(user);
+            // else res.status(401).json({ message: "Incorrect Password" });
         });
     } catch (err) {
         res.status(500).json({ message: err.message });
