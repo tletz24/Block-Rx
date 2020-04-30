2aaba8d0daa7efd3e69f0db4478d0893a142e2c3
Michael Sloma <syphr144@gmail.com>
2020-04-30 14:17:51 -0400

    readd connection and app

--- healthwallet-blockchain/healthwallet-rest-api/app.js
+++ healthwallet-blockchain/healthwallet-rest-api/app.js
@@ -18,10 +18,10 @@
 var log4js = require('log4js');
 log4js.configure({
 	appenders: {
-		out: { type: 'stdout' },
+	  out: { type: 'stdout' },
 	},
 	categories: {
-		default: { appenders: ['out'], level: 'info' },
+	  default: { appenders: ['out'], level: 'info' },
 	}
 });
 var logger = log4js.getLogger('NGOAPI');
@@ -30,7 +30,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var http = require('http');
 var util = require('util');
-var router = express.Router();
+var app = express();
 var cors = require('cors');
 var hfc = require('fabric-client');
 const uuidv4 = require('uuid/v4');
@@ -42,7 +42,7 @@ var blockListener = require('./blocklistener.js');
 
 hfc.addConfigFile('config.json');
 var host = 'localhost';
-var port = 4000;
+var port = 3000;
 var username = "member-healthwallet";
 var orgName = "";
 var channelName = hfc.getConfigSetting('channelName');
@@ -51,17 +51,18 @@ var peers = hfc.getConfigSetting('peers');
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// SET CONFIGURATIONS ///////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-router.options('*', cors());
-router.use(cors());
-router.use(bodyParser.json());
-router.use(bodyParser.urlencoded({
+app.options('*', cors());
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
 	extended: false
 }));
-router.use(function (req, res, next) {
-	logger.info(' ##### New request for URL %s', req.originalUrl);
+app.use(function(req, res, next) {
+	logger.info(' ##### New request for URL %s',req.originalUrl);
 	return next();
 });
-//wrrouterer to handle errors thrown by async functions. We can catch all
+
+//wrapper to handle errors thrown by async functions. We can catch all
 //errors thrown by async functions in a single place, here in this function,
 //rather than having a try-catch in every function below. The 'next' statement
 //used here will invoke the error handler function - see the end of this script
@@ -69,7 +70,7 @@ const awaitHandler = (fn) => {
 	return async (req, res, next) => {
 		try {
 			await fn(req, res, next)
-		}
+		} 
 		catch (err) {
 			next(err)
 		}
@@ -79,9 +80,9 @@ const awaitHandler = (fn) => {
 ///////////////////////////////////////////////////////////////////////////////
 //////////////////////////////// START SERVER /////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
-var server = http.createServer(router).listen(port, function () { });
+var server = http.createServer(app).listen(port, function() {});
 logger.info('****************** SERVER STARTED ************************');
-logger.info('***************  Listening on: http://%s:%s  ******************', host, port);
+logger.info('***************  Listening on: http://%s:%s  ******************',host,port);
 server.timeout = 240000;
 
 function getErrorMessage(field) {
@@ -109,32 +110,32 @@ wss.on('connection', function connection(ws) {
 ///////////////////////// REST ENDPOINTS START HERE ///////////////////////////
 ///////////////////////////////////////////////////////////////////////////////
 // Health check - can be called by load balancer to check health of REST API
-router.get('/health', awaitHandler(async (req, res) => {
+app.get('/health', awaitHandler(async (req, res) => {
 	res.sendStatus(200);
 }));
 
 // Register and enroll user. A user must be registered and enrolled before any queries 
 // or transactions can be invoked
-router.post('/users', awaitHandler(async (req, res) => {
+app.post('/users', awaitHandler(async (req, res) => {
 	logger.info('================ POST on Users');
-	username = req.body.username || 'michael';
-	orgName = req.body.orgName || 'health-wallet';
+	username = req.body.username;
+	orgName = req.body.orgName;
 	logger.info('##### End point : /users');
 	logger.info('##### POST on Users- username : ' + username);
 	logger.info('##### POST on Users - userorg  : ' + orgName);
 	let response = await connection.getRegisteredUser(username, orgName, true);
 	logger.info('##### POST on Users - returned from registering the username %s for organization %s', username, orgName);
-	logger.info('##### POST on Users - getRegisteredUser response secret %s', response.secret);
-	logger.info('##### POST on Users - getRegisteredUser response secret %s', response.message);
-	if (response && typeof response !== 'string') {
-		logger.info('##### POST on Users - Successfully registered the username %s for organization %s', username, orgName);
+    logger.info('##### POST on Users - getRegisteredUser response secret %s', response.secret);
+    logger.info('##### POST on Users - getRegisteredUser response secret %s', response.message);
+    if (response && typeof response !== 'string') {
+        logger.info('##### POST on Users - Successfully registered the username %s for organization %s', username, orgName);
 		logger.info('##### POST on Users - getRegisteredUser response %s', response);
 		// Now that we have a username & org, we can start the block listener
 		await blockListener.startBlockListener(channelName, username, orgName, wss);
 		res.json(response);
 	} else {
 		logger.error('##### POST on Users - Failed to register the username %s for organization %s with::%s', username, orgName, response);
-		res.json({ success: false, message: response });
+		res.json({success: false, message: response});
 	}
 }));
 
@@ -143,12 +144,12 @@ router.post('/users', awaitHandler(async (req, res) => {
  ************************************************************************************/
 
 // GET vaccinations
-router.get('/vaccinations', awaitHandler(async (req, res) => {
+app.get('/vaccinations', awaitHandler(async (req, res) => {
 	logger.info('================ GET on vaccinations');
 	let args = {};
 	let fcn = "queryAllVaccinations";
 
-	logger.info('##### GET on vaccinations - username : ' + username);
+    logger.info('##### GET on vaccinations - username : ' + username);
 	logger.info('##### GET on vaccinations - userOrg : ' + orgName);
 	logger.info('##### GET on vaccinations - channelName : ' + channelName);
 	logger.info('##### GET on vaccinations - chaincodeName : ' + chaincodeName);
@@ -157,12 +158,12 @@ router.get('/vaccinations', awaitHandler(async (req, res) => {
 	logger.info('##### GET on vaccinations - peers : ' + peers);
 	logger.info('##### GET on vaccinations - peers : ' + peers);
 
-	let message = await query.queryChaincode(peers, channelName, chaincodeName, args, fcn, username, orgName);
-	res.send(message);
+    let message = await query.queryChaincode(peers, channelName, chaincodeName, args, fcn, username, orgName);
+ 	res.send(message);
 }));
 
 // GET a specific vaccination
-router.get('/vaccinations/:vaccinationId', awaitHandler(async (req, res) => {
+app.get('/vaccinations/:vaccinationId', awaitHandler(async (req, res) => {
 	logger.info('================ GET on vac by vac ID');
 	logger.info('Username : ' + req.params);
 	let args = req.params;
@@ -180,19 +181,15 @@ router.get('/vaccinations/:vaccinationId', awaitHandler(async (req, res) => {
 	res.send(message);
 }));
 
-router.get('/patient-vaccinations/:patientID', awaitHandler(async (req, res) => {
-	// do our stuff 
-	next();
-}));
-
 // GET a specific patient vaccinations
-router.get('/patient-vaccinations/:patientID', awaitHandler(async (req, res) => {
+app.get('/patient-vaccinations/:patientID', awaitHandler(async (req, res) => {
 	logger.info('================ GET on vac by patient ID');
 	logger.info('Username : ' + req.params);
 	let args = req.params;
 	let fcn = "queryPatientVaccinations";
 
 	logger.info('##### GET on patient vaccinations by username - username : ' + username);
+	logger.info('##### GET on patient vaccinations by username - userOrg : ' + orgName);
 	logger.info('##### GET on patient vaccinations by username - channelName : ' + channelName);
 	logger.info('##### GET on patient vaccinations by username - chaincodeName : ' + chaincodeName);
 	logger.info('##### GET on patient vaccinations by username - fcn : ' + fcn);
@@ -204,7 +201,7 @@ router.get('/patient-vaccinations/:patientID', awaitHandler(async (req, res) => 
 }));
 
 // POST Donor
-router.post('/vaccinations', awaitHandler(async (req, res) => {
+app.post('/vaccinations', awaitHandler(async (req, res) => {
 	logger.info('================ POST on vaccinations');
 	var args = req.body;
 	var fcn = "createVaccination";
@@ -228,12 +225,12 @@ router.post('/vaccinations', awaitHandler(async (req, res) => {
 
 // GET details of a blockchain transaction using the record key (i.e. the key used to store the transaction
 // in the world state)
-router.get('/blockinfos/:docType/keys/:key', awaitHandler(async (req, res) => {
+app.get('/blockinfos/:docType/keys/:key', awaitHandler(async (req, res) => {
 	logger.info('================ GET on blockinfo');
 	logger.info('Key is : ' + req.params);
 	let args = req.params;
 	let fcn = "queryHistoryForKey";
-
+	
 	logger.info('##### GET on blockinfo - username : ' + username);
 	logger.info('##### GET on blockinfo - userOrg : ' + orgName);
 	logger.info('##### GET on blockinfo - channelName : ' + channelName);
@@ -251,8 +248,7 @@ router.get('/blockinfos/:docType/keys/:key', awaitHandler(async (req, res) => {
  * Error handler
  ************************************************************************************/
 
-router.use(function (error, req, res, next) {
+app.use(function(error, req, res, next) {
 	res.status(500).json({ error: error.toString() });
 });
 
-module.exports = router;
